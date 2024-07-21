fe4fa14721f1f08d95eb705708c74f636cdd314b
SkyblueVN <huyn09559@gmail.com>
2024-07-21 21:14:52 +0700

    code

--- main/video2.js
+++ main/video2.js
@@ -1,11 +1,8 @@
-const js = {
-    main: 2008,
-    func: function namsinh() {
-        return this.main
-    },
-};
-
-function jscript() {
-    return js.main
+function Test() {
+    let text = document.getElementById("test").innerHTML;
+    if (text.includes("HTML")) {
+        document.getElementById("test").innerHTML = text.replace("HTML", "JavaScript");
+    } else {
+        document.getElementById("test").innerHTML = text.replace("JavaScript", "HTML");
+    }
 }
-console.log(jscript())
